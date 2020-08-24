9aafd96d357517aed7c7481dd4987e7b2ae825a0
TilenOkretic <tilen.okretic@gmail.com>
2020-08-24 18:16:32 +0200

    inital commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Wllcome!</h1>
+      <p class="lead">This is a simple forum site!</p>
+      <hr class="my-4">
+      <p>Still in development!</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+  };
+
+</script>
